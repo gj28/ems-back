f64dc28f2e713974d303eff4fb80d26ea4800764
GauravJadhav33 <gjadhav4222.com>
2023-09-01 15:52:32 +0530

    IPv4

--- audit_logs.js
+++ audit_logs.js
@@ -1,7 +1,12 @@
-const db = require('./db'); 
+const db = require('./db');
+
+function extractIPv4(ipv6MappedAddress) {
+  const parts = ipv6MappedAddress.split(':');
+  return parts[parts.length - 1];
+}
 
 function log(req, res, next) {
-  const { method, url, body, ip } = req;
+  const { method, url, body } = req;
   const timestamp = new Date().toISOString();
   const entity = body.userType || 'User';
   const entityName = body.companyName || 'SenseLive';
@@ -9,11 +14,12 @@ function log(req, res, next) {
   const userType = req.body.designation || 'STD';
   const type = method;
   const status = res.statusCode >= 200 && res.statusCode < 400 ? 'successful' : 'failure';
+  const ipAddress = extractIPv4(req.ip); 
   const details = `URL: ${url}`;
 
   const logQuery = {
     text: 'INSERT INTO ems.logs (timestamp, ip, entity_type, entity_name, username, user_type, request_type, status, details) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-    values: [timestamp, ip, entity, entityName, user, userType, type, status, details],
+    values: [timestamp, ipAddress, entity, entityName, user, userType, type, status, details],
   };
 
   db.query(logQuery, (error, result) => {
@@ -28,18 +34,17 @@ function log(req, res, next) {
 
 function fetchLogs(req, res) {
   const query = 'SELECT * FROM ems.logs';
-  
+
   db.query(query, (error, result) => {
     if (error) {
       console.error('Error fetching logs:', error);
       return res.status(500).json({ message: 'Internal server error' });
     }
 
-    const rows = result.rows; 
+    const rows = result.rows;
 
     res.json(rows);
   });
 }
 
-
-module.exports = {log,fetchLogs};
+module.exports = { log, fetchLogs };
